2eab7ee43cebebc1dbc0b275e66f5711092a008a
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-07-16 14:49:15 +0330

    Created the routing in App.jsx(Main Component) Manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+// Importing Components
 import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Pricing from "./Routes/Pricing";
+import About from "./Routes/About";
+
 // Main Component
 const App = () => {
+    // Routing
+    let component = null;
+    // Switching between routes
+    switch (window.location.pathname) {
+        case "/":
+            component = <Home />;
+
+            break;
+        case "/pricing":
+            component = <Pricing />;
+            break;
+        case "/about":
+            component = <About />;
+            break;
+        default:
+            break;
+    }
+
     return (
         <div>
             <Header />
+            {component}
         </div>
     );
 };
