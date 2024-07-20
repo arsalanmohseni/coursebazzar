01dbc38e709448b5c8785d30d54fe4fdfa1a38fa
Arsalan Mohseni <arsalandeveloper961@gmail.com>
2024-07-20 14:24:17 +0330

    Created the footer and installed react-icons library

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,67 @@
+import { FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+
+const sections = [
+    {
+        title: "Solutions",
+        items: ["Marketing", "Analytics", "Commerce", "Data", "Cloud"],
+    },
+    {
+        title: "Support",
+        items: ["Pricing", "Documentation", "Guides", "API Status"],
+    },
+    {
+        title: "Company",
+        items: ["About", "Blog", "Jobs", "Press", "Partners"],
+    },
+    {
+        title: "Legal",
+        items: ["Claims", "Privacy", "Terms", "Policies", "Conditions"],
+    },
+];
+
+const items = [
+    { name: "Instagram", icon: FaInstagram, link: "https://www.instagram.com/" },
+    { name: "Twitter", icon: FaTwitter, link: "https://twitter.com/" },
+    { name: "Github", icon: FaGithub, link: "https://github.com/" },
+];
+
+const Footer = () => {
+    return (
+        <div className="py-y mt-24 w-full bg-slate-900 px-2 text-gray-300">
+            <div className="mx-auto grid max-w-[1240px] grid-cols-2 border-b-2 border-gray-600 py-8 md:grid-cols-6">
+                {sections.map((section, index) => (
+                    <div key={index}>
+                        <h6 className="pt-2 font-bold uppercase">{section.title}</h6>
+                        <ul>
+                            {section.items.map((item, i) => (
+                                <li key={i} className="py-1 text-gray-500 hover:text-white">
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+
+                <div className="col-span-2 pt-8 md:pt-2">
+                    <p className="font-bold uppercase">Subscribe to our newsletter</p>
+                    <p className="py-4">The latest news, articles, and resources, sent to your inbox weekly.</p>
+                    <form className="flex flex-col sm:flex-row">
+                        <input className="mb-4 mr-4 w-full rounded-md p-2" type="email" placeholder="Enter email.." />
+                        <button className="mb-4 p-2">Subscribe</button>
+                    </form>
+                </div>
+            </div>
+
+            <div className="mx-auto flex max-w-[1240px] flex-col justify-between px-2 py-4 text-center text-gray-500 sm:flex-row">
+                <p className="py-4">2022 Workflow, LLC. All rights reserved</p>
+                <div className="flex justify-between pt-4 text-2xl sm:w-[300px]">
+                    {items.map((x, index) => {
+                        return <x.icon key={index} className="hover:text-white" />;
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
